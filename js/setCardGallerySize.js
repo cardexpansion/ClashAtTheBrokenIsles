2fcde8c134fd9736a57a338f2067c3e8ead44776
cardexpansion <62172870+cardexpansion@users.noreply.github.com>
2020-04-09 21:54:11 +0300

    Add files via upload

--- js/setCardGallerySize.js
+++ js/setCardGallerySize.js
@@ -4,10 +4,11 @@ function setCardGallerySize(containerWidth, containerHeight)
 	
 	var allCards = document.getElementsByClassName("cardimage");
 	var cardAmount = allCards.length;	
+	var cardVertical = Math.ceil(cardAmount/5);
 	
 	var cardGallery = document.getElementById("cardGallery");
 	
-	cardGallery.style.marginTop = ((containerHeight - cardAmount*0.20*containerWidth*ratio*569/400)/2).toString() + 'px';
+	cardGallery.style.marginTop = ((containerHeight - cardVertical*containerWidth*ratio*569/400)/2).toString() + 'px';
 	
 	for (var i=0; i < cardAmount; i++)
 	{
